50965da5f003891e6d2c69b5a5a1bb02443c1316
bj thomas <redx318@gmail.com>
2016-06-17 21:23:25 +0000

    commit L45 files

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,20 @@
 class CommentsController < ApplicationController
   before_action :set_article
   def create
-   @comment = @article.comments.build(comment_params)
-   @comment.user = current_user
-   
-   if @comment.save
-     flash[:success] = "Comment has been created"
-   else
-    flash.now[:danger] = "Comment has not been created"
-   end
-   redirect_to article_path(@article)
+    unless current_user
+      flash[:danger] = "Please sign in or sign up first"
+      redirect_to new_user_session_path
+  else
+    @comment = @article.comments.build(comment_params)
+    @comment.user = current_user
+       
+    if @comment.save
+      flash[:success] = "Comment has been created"
+    else
+      flash.now[:danger] = "Comment has not been created"
+    end
+     redirect_to article_path(@article)
+    end
   end
   
   private
